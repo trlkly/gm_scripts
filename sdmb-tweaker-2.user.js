14c1ca2b09d6169c283b017c7381a926d76db204
trlkly <trlkly@hotmail.com>
2021-12-02 03:09:53 -0600

    Update sdmb-tweaker-2.user.js

--- sdmb-tweaker-2.user.js
+++ sdmb-tweaker-2.user.js
@@ -4,13 +4,14 @@
 // @version      2.0
 // @description  New tweaks for the new(er) Discourse SDMB
 // @author       BigTSDMB
-// @license      MIT
 // @updateURL    https://openuserjs.org/meta/BigTSDMB/SDMB_Tweaker_2.meta.js
 // @downloadURL  https://openuserjs.org/install/BigTSDMB/SDMB_Tweaker_2.user.js
+// @license      MIT
 // @match        https://boards.straightdope.com/*
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
+/* jshint esversion: 6 */
 
 function changeStyle(css, id = 'sdmb-tweaker') {
   if (!document.documentElement) {
@@ -34,13 +35,23 @@ function changeStyle(css, id = 'sdmb-tweaker') {
 let style = `
   .cooked a:not(.mention)/*, .d-editor-preview a:not(.mention)*/ {
      text-decoration: underline;
-     text-decoration-skip: trailing-spaces;
+     text-decoration-skip: trailing-spaces; /* currently doesn't work. used gradient workaround */
+  }
+  .cooked :not(.source) > a:not(.mention):visited, aside.onebox .onebox-body a[href]:visited {
+     color: var(--success-hover);
+  }
+  a:hover {
+     text-decoration: underline !important;
+  }
+  .title a, .menu-panel a:hover {
+     text-decoration: none !important;
   }
   .badge-notification.clicks {
-     left: -0.6ch;
+     left: -0.7ch;
      margin-right: -0.7ch;
      border-radius: 10px 10px 10px 0px;
-     background: radial-gradient(var(--primary-low) 68%, var(--secondary) 73%);
+     background: radial-gradient(67% 79%, var(--primary-low) 68%, transparent 73%),
+                 linear-gradient(to left, var(--primary-low) 60%, var(--secondary) 60%);
   }
   .MJXc-TeX-sans-R {
      font-family: var(--font-family) !important;
@@ -53,13 +64,18 @@ changeStyle(style);
 addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('[data-theme-name="straight dope light"]')) {
     changeStyle(`
-      a, a:active, a:hover { color: var(--tertiary-high); }
-      a:visited { color: #551a8b; } /* default visited color in most browsers*/
+      a, .cooked :not(.source) > a:active:not(.mention), /*aside.onebox .onebox-body a[href],*/
+      .cooked :not(.source) > a:hover:not(.mention) {
+         color: var(--tertiary-high);
+      }
+      .cooked :not(.source) > a:not(.mention):visited, aside.onebox .onebox-body a[href]:visited {
+         color: #551a8b; /* default visited color in most browsers */
+      }
     `, 'sdmb-tweaker-SDL');
   } else if (document.querySelector('[data-theme-name="minima"]')) {
     changeStyle(`
       .MJXc-TeX-sans-R {
-        font-family: sans-serif !important;
+         font-family: sans-serif !important;
       }
     `, 'sdmb-tweaker-Min');
   }
@@ -71,5 +87,9 @@ addEventListener('DOMContentLoaded', () => {
 });
 
 function main_script(mutation) {
+  document.querySelectorAll('.badge-notification.clicks').forEach( badge => {
+    let spacer = badge.previousSibling;
+    if(spacer.textContent == ' ') { spacer.textContent = ''; }
+  });
 }
 */
